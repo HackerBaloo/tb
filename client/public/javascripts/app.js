a38d27d35cbd9ed52ed88b1ffb9b1bbc3b214e2a
Björn Carlsson <b.r.carlsson@gmail.com>
2017-02-18 23:21:55 +0100

    now possible to select and update

--- client/public/javascripts/app.js
+++ client/public/javascripts/app.js
@@ -3,11 +3,13 @@ const route = '/api/v1/users';
 angular.module('nodeUsers', [])
     .controller('mainController', ($scope, $http) => {
         $scope.formData = {};
-        $scope.userData = {};
+        $scope.usersData = {};
         // Get all users
         $http.get(route)
             .success((users) => {
                 $scope.usersData = users;
+                $scope.formData.DisableUpdate = true;
+                $scope.formData.DisableCreate = false;
                 console.log(users);
             })
             .error((error) => {
@@ -18,6 +20,24 @@ angular.module('nodeUsers', [])
             $http.post(route, $scope.formData)
                 .success((data) => {
                     $scope.formData = {};
+                    $scope.formData.DisableUpdate = true;
+                    $scope.formData.DisableCreate = false;
+                    $scope.usersData = data;
+                    console.log(data);
+                })
+                .error((error) => {
+                    console.log('Error: ' + error);
+                });
+        };
+        // Update a user
+        $scope.updateUser = (userId) => {
+            console.log('Updating Userid:' + userId)
+            console.log('Updating User:' + $scope.formData.name)
+            $http.put(route + '/' + userId, $scope.formData)
+                .success((data) => {
+                    $scope.formData = {};
+                    $scope.formData.DisableUpdate = true;
+                    $scope.formData.DisableCreate = false;
                     $scope.usersData = data;
                     console.log(data);
                 })
@@ -35,6 +55,28 @@ angular.module('nodeUsers', [])
                 .error((data) => {
                     console.log('Error: ' + data);
                 });
+        };
+        // Select a user
+        $scope.selectUser = (userID) => {
+            console.log('Select UserId:' + userID)
+            for (i = 0; i < $scope.usersData.length; i++) {
+                console.log('Checking UserId:' + $scope.usersData[i].id)
+                if (userID == $scope.usersData[i].id) {
+                    user = $scope.usersData[i];
+                    console.log('Selected:' + user.name)
+                    $scope.formData = user;
+                    console.log('Selected formData:' + $scope.formData.id)
+                    $scope.formData.DisableUpdate = false;
+                    $scope.formData.DisableCreate = true;
+                }
+            }
+        };
+        // Unselect a user
+        $scope.unselectUser = () => {
+            console.log('UnSelect')
+            $scope.formData = {};
+            $scope.formData.DisableUpdate = true;
+            $scope.formData.DisableCreate = false;
         };
     });
 
