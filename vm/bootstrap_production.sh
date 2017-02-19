bcdacb09e9220fda85c1261da4455175a4d48cf6
Björn Carlsson <b.r.carlsson@gmail.com>
2017-02-19 19:01:22 +0100

    now a working vm built

---
+++ vm/bootstrap_production.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+localectl set-locale LANG="en_US.UTF-8"
+
+cp /vagrant/.pgpass .
+
+cd /vagrant/
+
+#install postgres client
+sudo apt install -y postgresql-client-common
+sudo apt install -y postgresql-client
+
+#start postgres
+sudo docker run -p 5432:5432 --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+
+# create users table
+psql -h 172.17.0.2 -U postgres -c "CREATE TABLE users(id SERIAL PRIMARY KEY, name text, address text, email text UNIQUE not null, birthday date not null);"
+
+#www
+docker build -t www .
+
+docker run -p 3030:3030 --name www --link postgres:postgres -d www
