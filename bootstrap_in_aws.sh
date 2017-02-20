f86346d8f03d9aeec48417114b13732de116731a
Björn Carlsson <b.r.carlsson@gmail.com>
2017-02-20 14:09:33 +0100

    easier aws deploy

---
+++ bootstrap_in_aws.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+localectl set-locale LANG="en_US.UTF-8"
+
+cp vm/.pgpass ~/
+
+#install postgres client
+sudo apt install -y postgresql-client-common
+sudo apt install -y postgresql-client
+
+#start postgres
+docker run -p 5432:5432 --name postgres -e POSTGRES_PASSWORD=mysecretpassword -d postgres
+
+# create users table
+psql -h 172.17.0.2 -U postgres -c "CREATE TABLE users(id SERIAL PRIMARY KEY, name text, address text, email text UNIQUE not null, birthday date not null);"
+
+#www
+docker build -t www .
+
+docker run -p 80:3030 --name www --link postgres:postgres -d www
--- create_ami.sh
+++ create_ami.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-folder=aws/
+folder=~/aws/
 
 cp bootstrap_production.sh $folder
 cp Dockerfile $folder
